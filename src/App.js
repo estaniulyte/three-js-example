30b8a38d4e34b3fd082e7ebd96211c54d633d11d
estaniulyte <e.staniulyte98@gmail.com>
2022-05-17 18:37:01 +0300

    Add router

--- src/App.js
+++ src/App.js
@@ -9,37 +9,41 @@ import Sphere from './components/AnimatedSphere';
 
 import WordsSphere from './components/WordsSphere';
 
+import { BrowserRouter } from 'react-router-dom';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://threejs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn ThreeJS
-        </a>
-        <Canvas className="canvas">
-          <OrbitControls enableZoom={false} />
-          <ambientLight intensity={0.5} />
-          <directionalLight position={[-2, 5, 2]} intensity={1} />
-          <Suspense fallback={null}>
-            <Box />
-          </Suspense>
-        </Canvas>
-        <Canvas className="canvas">
-          <OrbitControls enableZoom={false} />
-          <ambientLight intensity={0.5} />
-          <directionalLight position={[-2, 5, 2]} intensity={1} />
-          <Suspense fallback={null}>
-            <Sphere />
-          </Suspense>
-        </Canvas>
-        <WordsSphere />
-      </header>
-    </div>
+    <BrowserRouter basename="/three-js-example">
+      <div className="App">
+        <header className="App-header">
+          <a
+            className="App-link"
+            href="https://threejs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn ThreeJS
+          </a>
+          <Canvas className="canvas">
+            <OrbitControls enableZoom={false} />
+            <ambientLight intensity={0.5} />
+            <directionalLight position={[-2, 5, 2]} intensity={1} />
+            <Suspense fallback={null}>
+              <Box />
+            </Suspense>
+          </Canvas>
+          <Canvas className="canvas">
+            <OrbitControls enableZoom={false} />
+            <ambientLight intensity={0.5} />
+            <directionalLight position={[-2, 5, 2]} intensity={1} />
+            <Suspense fallback={null}>
+              <Sphere />
+            </Suspense>
+          </Canvas>
+          <WordsSphere />
+        </header>
+      </div>
+    </BrowserRouter>
   );
 }
 
